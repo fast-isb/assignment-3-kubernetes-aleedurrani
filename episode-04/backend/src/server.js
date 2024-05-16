c1acb5d4d8833e69923f879b77c2a3d39eab4818
Your Name <you@example.com>
2024-05-16 12:25:40 +0500

    Dockerfiles, Yaml files and Presistent Volume

--- episode-04/backend/src/server.js
+++ episode-04/backend/src/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const routes =require('./routes');
 const path = require("path");
 const app = express();
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 3003
 
 app.use(cors())
 app.use(express.json())
